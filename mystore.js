2c17c3a2ecc117ca8fce063c77d1a471700474b6
Marte Løge <marte.loge@gmail.com>
2017-06-01 18:34:43 +0200

    Update mystore.js

--- mystore.js
+++ mystore.js
@@ -1,1 +1,27 @@
+var isProductPage = (document.location.pathname).match('/products');
+var url = "https://www.listfully.org/add"
+var storeId = "123456789"
+var props = {
+  'productImage': 'image',
+  'productPrice': 'price',
+  'productTitle': 'name'
+}
 
+
+var generateListfullyLink = function(url, storeId, props) {
+  var link = url + "?code=" + storeId;
+
+  if ($("meta[name='description']").attr('content') !== null) {
+     link = link + "&description=" + $("meta[name='description']").attr('content')
+  }
+
+  $.each( props, (key, value) => {
+    link = link + "&" + value + "=" + $('article').data(key)
+  });
+
+  return link;
+}
+
+if (!!isProductPage) {
+  generateListfullyLink(url, storeId, props);
+}
