894842c504fef685b7ee39ec448a5a9d21c0934f
Marte Løge <marte.loge@gmail.com>
2017-07-06 00:42:41 +0200

    Update 24.js

--- 24.js
+++ 24.js
@@ -9,45 +9,47 @@ var props = {
   'currency': 'product:price:currency',
   'image': 'og:image',
   'name': 'og:title',
-  'description': 'og:description',
-  'url': 'og:url'
+  'description': 'og:description'
 };
 
-var generateLinkCart = function(url) {
-  var cartLink = window.location.origin + "/handlevogn/fill?products=";
-  
-  var productId = $("input[name='id']").val();
-  if (productId) { cartLink = cartLink + productId; }
-  else { return url; }
-  
-  var details = $("div .product__attributes").find("select").val();
-  if (details) { cartLink = cartLink + "." + details; }
-  
-  return cartLink
-}
+var generateNewLink = function(generatedLink) {
+  var newLink = generatedLink;
 
+  newLink = newLink + "&url=" + window.location.origin + "/handlevogn/fill?products=";
+
+  var productId = $("input[name='id']").val();
+  if (productId) { newLink = newLink + productId; }
+  else { return url; }
+
+  var details = $("div .product__attributes").find("select");
+  if (details) {
+    $.each( details, function( key, value ) {
+      newLink = newLink + "." + $(value).val();
+    });
+  }
+
+  return newLink;
+}
 
 var generateListfullyLink = function(url, storeId, props) {
   var link = url + "?code=" + storeId + "&store=" + storeName;
-  
+
   if (currency) {
     link = link + "&currency" + currency;
   }
-  
+
   var quantity = $("input[name='antall']").val();
   if (quantity) {
     link = link + "&quantity=" + quantity;
   } else {
     link = link + "&quantity=1";
   }
-  
+
   $.each( props, (key, value) => {
     if($("meta[property='" + value + "']").attr('property') !== null) {
        var propValue = $("meta[property='" + value + "']").attr('content');
        if (value === "product:price:amount" && !isNaN(propValue)) {
          link = link + "&" + key + "=" + parseInt(propValue);
-       } else if (value === "og:url" && propValue) {
-         link = link + "&" + key + "=" + generateLinkCart(propValue)
        } else {
          link = link + "&" + key + "=" + encodeURIComponent(propValue);
        }
@@ -59,9 +61,17 @@ var generateListfullyLink = function(url, storeId, props) {
 
 if (!!isProductPage) {
   var link = generateListfullyLink(url, storeId, props);
-  
   $( "a.product__wishlist-button" ).replaceWith(
-    "<a target='_blank' href='" + link + "' id='listfully' class='button large expand secondary product__wishlist-button disabled'>" +
+    "<a target='_blank' href='" + generateNewLink(link) +
+    "' id='listfully' class='button large expand secondary product__wishlist-button disabled'>" +
     "<i class='icon-favorite'></i>Ønskeliste</a>"
   );
+
+  $( "select" ).change(function() {
+    $("#listfully").attr("href", generateNewLink(link));
+  });
+
+  $( "input#buy_count" ).change(function() {
+    $("#listfully").attr("href", generateNewLink(link));
+  });
 }
