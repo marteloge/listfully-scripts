7049fc2030446dac337cb6e00dca589c3f63844a
Marte Løge <marte.loge@gmail.com>
2017-07-05 21:18:10 +0200

    Update shop-24.js

--- shop-24.js
+++ shop-24.js
@@ -1,1 +1,9 @@
-
+var storeId = "123456"
+var props = {
+  'price': 'product:price:amount',
+  'currency': 'product:price:currency',
+  'image': 'og:image',
+  'name': 'og:title',
+  'description': 'og:description',
+  'url': 'og:url'
+};
