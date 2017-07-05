7d1ce41e9a8583639d34f525837bc96289a74dba
Marte Løge <marte.loge@gmail.com>
2017-07-05 21:20:22 +0200

    Update 24.js

--- 24.js
+++ 24.js
@@ -1,14 +1,5 @@
 var isProductPage = (document.location.pathname).match('/produkt');
 var url = "https://www.listfully.org/add"
-var storeId = "1628294150"
-var props = {
-  'price': 'product:price:amount',
-  'currency': 'product:price:currency',
-  'image': 'og:image',
-  'name': 'og:title',
-  'description': 'og:description',
-  'url': 'og:url'
-};
 
 var generateLinkCart = function(url) {
   var cartLink = window.location.origin + "/handlevogn/fill?products=";
