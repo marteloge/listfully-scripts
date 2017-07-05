0ece8336b3afc81157f6be0facf1fa5c767b441f
Marte Løge <marte.loge@gmail.com>
2017-07-05 21:30:08 +0200

    Update shop-24-beeco.js

--- shop-24-beeco.js
+++ shop-24-beeco.js
@@ -1,9 +1,2 @@
 var storeId = "1628294150"
-var props = {
-  'price': 'product:price:amount',
-  'currency': 'product:price:currency',
-  'image': 'og:image',
-  'name': 'og:title',
-  'description': 'og:description',
-  'url': 'og:url'
-};
+var storeName = "Be:Eco";
