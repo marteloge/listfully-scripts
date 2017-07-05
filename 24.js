2c120531b8c8df6e358014200f59b8620c5ab123
Marte Løge <marte.loge@gmail.com>
2017-07-05 20:35:10 +0200

    Update 24.js

--- 24.js
+++ 24.js
@@ -10,16 +10,32 @@ var props = {
   'url': 'og:url'
 };
 
+var generateLinkCart = function(url) {
+  var product = $("input[name='id']").val();
+  var details = $("div .product__attributes").find("select").val();
+  var cartLink = window.location.origin + "/handlevogn/fill?products=" + product;
+  if (details) { cartLink = cartLink + "." + details; }
+  return cartLink
+}
+
+
 var generateListfullyLink = function(url, storeId, props) {
   var link = url + "?code=" + storeId;
   
-  link = link + "&quantity=1";
-
+  var quantity = $("input[name='antall']").val();
+  if (quantity) {
+    link = link + "&quantity=" + quantity;
+  } else {
+    link = link + "&quantity=1";
+  }
+  
   $.each( props, (key, value) => {
     if($("meta[property='" + value + "']").attr('property') !== null) {
        var propValue = $("meta[property='" + value + "']").attr('content');
        if (value === "product:price:amount" && !isNaN(propValue)) {
          link = link + "&" + key + "=" + parseInt(propValue);
+       if (value === "og:url" && propValue) {
+         link = link + "&" + key + "=" + generateLinkCart()
        } else {
          link = link + "&" + key + "=" + encodeURIComponent(propValue);
        }
