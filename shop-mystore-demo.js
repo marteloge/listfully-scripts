109acccaaa04e99d0db8a080273bec0b398bda8b
Marte Løge <marte.loge@gmail.com>
2017-07-21 01:19:29 +0200

    Update shop-mystore-demo.js

--- shop-mystore-demo.js
+++ shop-mystore-demo.js
@@ -3,3 +3,9 @@ var website = "http://listfully.demo.mystore.no";
 var imageURL = window.location.origin + "/users/listfully_mystore_no/images/";
 var storeName = "MyStore";
 var currency = "NOK";
+var props = {
+  'og:image': 'image',
+  'og:title': 'name',
+  'og:description': 'description',
+  'product:price:amount': 'price'
+}
