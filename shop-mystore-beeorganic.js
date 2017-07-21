f598ce029cea33807195b84eb07890d00698d631
Marte Løge <marte.loge@gmail.com>
2017-07-21 12:38:54 +0200

    Update shop-mystore-beeorganic.js

--- shop-mystore-beeorganic.js
+++ shop-mystore-beeorganic.js
@@ -2,10 +2,3 @@ var storeId = "1549723083";
 var website = "https://www.beeorganic.no";
 var storeName = "BeeOrganic";
 var currency = "NOK";
-
-var link =
-  "https://www.listfully.no/add?" +
-  "code=" + storeId +
-  "&store=" + storeName +
-  "&website=" + website +
-  "&currency" + currency;
