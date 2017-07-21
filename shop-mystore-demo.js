de6fd2683baeedbc1fb10f5c48126e30b3c4e026
Marte Løge <marte.loge@gmail.com>
2017-07-21 12:41:10 +0200

    Update shop-mystore-demo.js

--- shop-mystore-demo.js
+++ shop-mystore-demo.js
@@ -2,10 +2,4 @@ var storeId = "123456789";
 var website = "http://listfully.demo.mystore.no";
 var storeName = "Demobutikk";
 var currency = "NOK";
-
-var link =
-  "https://www.listfully.org/add?" +
-  "code=" + storeId +
-  "&store=" + storeName +
-  "&website=" + website +
-  "&currency=" + currency;
+var product_image = "";
