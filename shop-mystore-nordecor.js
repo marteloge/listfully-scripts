8364e070066b8186b8cb48426e582b22bb82fd2a
Marte Løge <marte.loge@gmail.com>
2017-07-21 04:01:22 +0200

    Update shop-mystore-nordecor.js

--- shop-mystore-nordecor.js
+++ shop-mystore-nordecor.js
@@ -1,1 +1,11 @@
-4345540281
+var storeId = "4345540281";
+var website = "http://listfully.demo.mystore.no";
+var storeName = "Demobutikk";
+var currency = "NOK";
+
+var link =
+  "https://www.listfully.org/add?" +
+  "code=" + storeId +
+  "&store=" + storeName +
+  "&website=" + website +
+  "&currency=" + currency;
