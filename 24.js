d2c7a8d6fee2dd19d34cd9ab3fb1cdde72f336c0
Marte Løge <marte.loge@gmail.com>
2017-07-22 15:09:08 +0200

    Update 24.js

--- 24.js
+++ 24.js
@@ -4,7 +4,6 @@ var isProductPage = (document.location.pathname).match('/produkt');
 var url = "https://www.listfully.org/add";
 var props = {
   'price': 'product:price:amount',
-  'currency': 'product:price:currency',
   'image': 'og:image',
   'name': 'og:title',
   'description': 'og:description'
