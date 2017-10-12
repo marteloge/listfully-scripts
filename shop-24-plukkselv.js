c1e5d825f99b665423a4e9cd60355e67506b184c
Marte Løge <marte.loge@gmail.com>
2017-10-12 10:31:18 +0200

    Update shop-24-plukkselv.js

--- shop-24-plukkselv.js
+++ shop-24-plukkselv.js
@@ -1,1 +1,11 @@
+var storeId = "8098125695"
+var storeName = "Plukkselv";
+var currency = "NOK";
+var website = "https://plukkselv.no";
 
+var props = {
+  'price': 'product:price:amount',
+  'image': 'og:image',
+  'name': 'og:title',
+  'description': 'og:description'
+};
