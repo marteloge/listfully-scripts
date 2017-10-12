3cc7cc583881627958c57228257bb136ecf13658
Marte Løge <marte.loge@gmail.com>
2017-10-12 10:41:15 +0200

    Update shop-24-aktivebarn.js

--- shop-24-aktivebarn.js
+++ shop-24-aktivebarn.js
@@ -1,1 +1,11 @@
+var storeId = "0500986163"
+var storeName = "Aktivebarn";
+var currency = "NOK";
+var website = "https://aktivebarn.no";
 
+var props = {
+  'price': 'product:price:amount',
+  'image': 'og:image',
+  'name': 'og:title',
+  'description': 'og:description'
+};
